d2f8975e4453e8464e8929accc81ce047db6d004
smile308 <70571820+smile308@users.noreply.github.com>
2023-04-24 18:50:40 +0900

    2023-04-24

---
+++ src/UserInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./UserInput.css";
+
+const UserInput = () => {
+  return (
+    <div>
+      <div>Username</div>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default UserInput;
