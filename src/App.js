52fe9dd4baa806549768f2e3afbd6620136ae4c3
smile308 <70571820+smile308@users.noreply.github.com>
2023-04-29 13:31:01 +0900

    2023-04-29

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
 
+const UserInfo = [{ id: "", username: "", age: "" }];
+
 function App() {
-  const [name, setName] = useState("");
-  const nameChangeHandler = () => {
-    setName();
+  const [data, setData] = useState(UserInfo);
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const newData = {
+      username: enteredName,
+      age: +enteredAge,
+      id: Math.random(),
+    };
+
+    setData(newData, ...data);
+    setEnteredName("");
+    setEnteredAge("");
   };
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <div>Username</div>
-        <input type="text" onChange={nameChangeHandler} />
+        <input type="text" value={enteredName} onChange={nameChangeHandler} />
         <div>Age(Years)</div>
-        <input type="number" />
+        <input type="number" value={enteredAge} onChange={ageChangeHandler} />
       </form>
       <button type="submit">Add User</button>
     </div>
