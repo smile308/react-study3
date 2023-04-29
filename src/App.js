09d89713c2deacd3b01030865b6ba436ada7cd24
smile308 <70571820+smile308@users.noreply.github.com>
2023-04-30 06:03:24 +0900

    2023-04-30

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
-const UserInfo = [{ id: "", username: "", age: "" }];
+import AddUser from "./components/Users/AddUsers";
 
 function App() {
-  const [data, setData] = useState(UserInfo);
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-  const nameChangeHandler = (event) => {
-    setEnteredName(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const newData = {
-      username: enteredName,
-      age: +enteredAge,
-      id: Math.random(),
-    };
-
-    setData(newData, ...data);
-    setEnteredName("");
-    setEnteredAge("");
-  };
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <div>Username</div>
-        <input type="text" value={enteredName} onChange={nameChangeHandler} />
-        <div>Age(Years)</div>
-        <input type="number" value={enteredAge} onChange={ageChangeHandler} />
-      </form>
-      <button type="submit">Add User</button>
+      <AddUser />
     </div>
   );
 }
